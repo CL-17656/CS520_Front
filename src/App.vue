2e26b50086970ec3d31308640d95b27706dd6ddc
CL-17656 <110571571+CL-17656@users.noreply.github.com>
2024-11-16 19:40:21 -0500

    Add start page

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <nav>
       <RouterLink :to="{name: 'studenthome'}" class="nav-link">Student Home</RouterLink>
       <RouterLink :to="{name: 'instructorhome'}" class="nav-link">Instructor Home</RouterLink>
+      <RouterLink :to="{name: 'start'}" class="nav-link">Start</RouterLink>
       <div>
         <RouterLink :to="{name : 'register'}" class="nav-link">Register</RouterLink>
       </div>
