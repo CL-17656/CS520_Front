93e3a2c78c7862109e5a1e91c594bd75f8e67f46
bertiesh <bertiesh98@gmail.com>
2024-11-26 19:22:59 -0500

    fix

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL: 'http://localhost:8898', // I hope this where the backend will be hosted as usually we host it on 8k
   headers: {
-    'Content-Type': 'application/json',
+    // 'Content-Type': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
   },
 });
 
