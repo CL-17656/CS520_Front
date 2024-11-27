8984a4be7b5508917f07910aa4ed29d311f64d74
CL-17656 <110571571+CL-17656@users.noreply.github.com>
2024-11-26 19:27:14 -0500

    fix

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -4,30 +4,12 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL: 'http://localhost:8898', // I hope this where the backend will be hosted as usually we host it on 8k
   headers: {
-    // 'Content-Type': 'application/json',
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
-});
-
-const apiLoginClient = axios.create({
-  baseURL: 'http://localhost:8898', // I hope this where the backend will be hosted as usually we host it on 8k
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Type': 'application/json',
   },
 });
 
 // This is the Interceptor to include the token in every request
 apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('authToken');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
-
-apiLoginClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('authToken');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
