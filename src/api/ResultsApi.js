338c0473295e426339c25f21c67788471e58840a
Arin1511 <162939451+Arin1511@users.noreply.github.com>
2024-11-29 02:21:57 -0500

    Update ResultsApi.js

--- src/api/ResultsApi.js
+++ src/api/ResultsApi.js
@@ -1,1 +1,12 @@
+import apiClient from './axiosConfig';
+
+export async function fetchResults(projectId) {
+  try {
+    const response = await apiClient.get(`/project/${projectId}/history`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching results:', error);
+    throw error;
+  }
+}
 
