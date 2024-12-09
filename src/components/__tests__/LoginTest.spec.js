0981b24c96283080d83c933acb55aa011d19eae0
CL-17656 <110571571+CL-17656@users.noreply.github.com>
2024-12-09 18:38:37 -0500

    test update

--- src/components/__tests__/LoginTest.spec.js
+++ src/components/__tests__/LoginTest.spec.js
@@ -3,16 +3,6 @@ import { setActivePinia, createPinia } from 'pinia';
 
 import { mount } from '@vue/test-utils'
 import { useAuthenticationStore } from '@/stores/Auth';
-//import LoginView from '@/views/Auth/LoginView.vue'
-/*
-import HelloWorld from '../HelloWorld.vue'
-
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})*/
 import LoginView from '@/views/Auth/LoginView.vue'; 
 
 describe('LoginView', () => {
@@ -28,5 +18,32 @@ describe('LoginView', () => {
     await input.setValue('Vitest');
 
     expect(input.element.value).toBe('Vitest');
+  });
+
+  it('updates password input on input change', async () => {
+    const wrapper = mount(LoginView);
+
+    const input = wrapper.find('[type="password"]');
+    await input.setValue('Vitest');
+
+    expect(input.element.value).toBe('Vitest');
+  });
+
+  it('updates username input updates submitData', async () => {
+    const wrapper = mount(LoginView);
+
+    const input = wrapper.find('[type="text"]');
+    await input.setValue('Vitest');
+
+    expect(wrapper.vm.loginData.username).toBe('Vitest');
+  });
+
+  it('updates password input updates submitData', async () => {
+    const wrapper = mount(LoginView);
+
+    const input = wrapper.find('[type="password"]');
+    await input.setValue('Vitest');
+
+    expect(wrapper.vm.loginData.password).toBe('Vitest');
   });
 });
