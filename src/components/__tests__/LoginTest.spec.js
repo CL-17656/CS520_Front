5321fbf64060160a0b139afedbd45b6b0f28f911
CL-17656 <110571571+CL-17656@users.noreply.github.com>
2024-12-09 19:16:09 -0500

    more login test

--- src/components/__tests__/LoginTest.spec.js
+++ src/components/__tests__/LoginTest.spec.js
@@ -46,4 +46,42 @@ describe('LoginView', () => {
 
     expect(wrapper.vm.loginData.password).toBe('Vitest');
   });
+
+  it('error correctly displayed for username on empty submit', async () => {
+    const wrapper = mount(LoginView)
+    await wrapper.find('form').trigger('submit.prevent')
+    expect(wrapper.vm.errors.username.length).toBe(1)
+    expect(wrapper.vm.errors.username[0]).toBe('Must enter an username')
+  })
+
+  it('error correctly displayed for password on empty submit', async () => {
+    const wrapper = mount(LoginView)
+    await wrapper.find('form').trigger('submit.prevent')
+    expect(wrapper.vm.errors.password.length).toBe(1)
+    expect(wrapper.vm.errors.password[0]).toBe('Must enter a password')
+  })
+
+  it('error correctly displayed for only empty username submit', async () => {
+    const wrapper = mount(LoginView)
+
+    const input = wrapper.find('[type="password"]');
+    await input.setValue('Vitest');
+
+    await wrapper.find('form').trigger('submit.prevent')
+    expect(wrapper.vm.errors.username.length).toBe(1)
+    expect(wrapper.vm.errors.username[0]).toBe('Must enter an username')
+    expect(wrapper.vm.errors.password.length).toBe(0)
+  })
+
+  it('error correctly displayed for only empty password submit', async () => {
+    const wrapper = mount(LoginView)
+
+    const input = wrapper.find('[type="text"]');
+    await input.setValue('Vitest');
+
+    await wrapper.find('form').trigger('submit.prevent')
+    expect(wrapper.vm.errors.username.length).toBe(0)
+    expect(wrapper.vm.errors.password.length).toBe(1)
+    expect(wrapper.vm.errors.password[0]).toBe('Must enter a password')
+  })
 });
